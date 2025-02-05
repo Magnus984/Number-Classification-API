21c3299874bed10b9572e80f604fee3d21e6007c
Magnus <nharnharyhaw984@gmail.com>
2025-02-05 16:30:33 +0000

    Enable CORS for API and remove debug mode from app run

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, jsonify
 from flask_restful import Api
 from api.v1.routes.classify_number import ClassifyNumber
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 app.json.sort_keys = False
 api = Api(app)
 api.add_resource(ClassifyNumber, '/api/classify-number')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
